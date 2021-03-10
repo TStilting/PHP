928250c14f9744213d149fe9e2047778c90c3977
Troy <tstilting@roc-dev.com>
2021-03-10 10:46:02 +0100

    H2 gedaan

---
+++ H2/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+
+echo $a.". Ik heb alleen ".$c." manieren gevonden ".$b.".";
+
+$d = "die geen fout";
+$e = "en";
+$f = "niet";
+
+echo "Mens".$e." ".$d.$e." maken, werk".$e." ".$f.".";
+
+$g = "ry";
+$h = ", but";
+$i = "fail";
+
+echo "T".$g." and ".$i.$h." never ".$i." to T".$g.".";
+
+$j = "\"omgaan";
+$k = "met";
+$l = "teleu";
+
+echo "De cursus ".$j." ".$k." ".$l."rstelling\" kan vanavond helaas niet doorgaan.";
