75bf8acf4ead82404ec640003f46bd0cbd80bccc
Troy <tstilting@roc-dev.com>
2021-03-15 11:14:03 +0100

    H3 aan gewerkt

---
+++ H3/index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Array</title>
+</head>
+ <body>
+ <?php
+  for($foto = 1; $foto < 9; $foto++) {
+   echo "<img src='../Fotos/Turtle".$foto.".jpg'";
+  }
+  ?>
+ </body>
+</html>
+
+
+
+
