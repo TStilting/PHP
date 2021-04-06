37cc362eb6ab17747e3b803147888010598c2cae
Troy <tstilting@roc-dev.com>
2021-04-06 11:22:44 +0200

    verder aan de volgende opdracht in H5 gewerkt

--- H5/resultaat3.php
+++ H5/resultaat3.php
@@ -1,3 +1,15 @@
 <?php
 
+$gegevens = array("piet@worldonline.nl" => "doetje123", "klaas@carpets.nl" => "snoepje777",
+    "truushendriks@wegweg.nl" => "arkiearkie201");
+
+
+foreach($gegevens as $emaal => $wachtwurd) {
+    if ($_POST['emaal'] != $_POST['wachtwurd']) {
+        print "Geen Toegang <br>";
+        print "<a href='form3.html'>Opnieuw Invullen</a><br>";
+    } else {
+        print "toegang verleend";
+    }
+}
 ?>
