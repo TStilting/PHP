56de15ec53dd6e8dcc4c2c10dc7f8d4a4df63b76
Troy <tstilting@roc-dev.com>
2021-04-12 11:13:48 +0200

    H6 begonnen

--- H6/index.php
+++ H6/index.php
@@ -1,1 +1,25 @@
 <?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>foooorm</title>
+</head>
+<body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=schoool", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
+</body>
+</html>
